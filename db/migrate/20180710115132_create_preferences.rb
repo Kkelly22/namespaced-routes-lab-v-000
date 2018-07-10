4079dbca09772898d5311b367c9aaa7f21fd76f8
Kaitlin Kelly <kk504408@gmail.com>
2018-07-10 11:53:03 +0000

    automatically backed up by learn

--- db/migrate/20180710115132_create_preferences.rb
+++ db/migrate/20180710115132_create_preferences.rb
@@ -1,7 +1,8 @@
 class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
-      t.integer :sort
+      t.string :song_sort
+      t.string :artist_sort
       t.timestamps null: false
     end
   end
